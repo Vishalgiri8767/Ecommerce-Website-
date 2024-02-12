0f56262fd730c78d90fcff15217ee62c9eca0448
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-02-12 23:58:24 +0530

    Added login and signup pages for initial user interface setup

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -12,8 +12,6 @@ export const Login = () => {
                 <div>
                     <input type="email"
                         name='email'
-                        //   value={email}
-                        //    onChange={(e) => setEmail(e.target.value)}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
                     />
@@ -21,15 +19,12 @@ export const Login = () => {
                 <div>
                     <input
                         type="password"
-                        //     value={password}
-                        //       onChange={(e) => setPassword(e.target.value)}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
                 </div>
                 <div className=' flex justify-center mb-3'>
                     <button
-                        //      onClick={signin}
                         className=' bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
                         Login
                     </button>
