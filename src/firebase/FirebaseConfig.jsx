b9d7473660a098d821e472b668e625b586087778
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-02-17 00:18:46 +0530

    firebase initilised for user authentication

---
+++ src/firebase/FirebaseConfig.jsx
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCP2ZFadGFxxc0PEiATIMYiqRLQPJy0Vbk",
+  authDomain: "e-commerce-web-app-2d378.firebaseapp.com",
+  projectId: "e-commerce-web-app-2d378",
+  storageBucket: "e-commerce-web-app-2d378.appspot.com",
+  messagingSenderId: "826817630143",
+  appId: "1:826817630143:web:bacbd4c57306a6b66bdb0a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
