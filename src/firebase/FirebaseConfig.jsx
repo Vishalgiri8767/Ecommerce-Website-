f29447d6a0d05288efb11c54b57ff16c59393440
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-02-17 00:31:36 +0530

    firebase configuration setup

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCP2ZFadGFxxc0PEiATIMYiqRLQPJy0Vbk",
@@ -12,3 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+ const fireDB = getFirestore(app);
+ const auth = getAuth(app);
+
+ export  {fireDB, auth};
