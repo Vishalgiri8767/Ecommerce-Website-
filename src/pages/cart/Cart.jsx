e10fcfbfbd4d931069ed499e57a3abe7ea745801
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-02-19 23:10:58 +0530

    Added shipping charges calculation

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -25,13 +25,6 @@ function Cart() {
     localStorage.setItem('cart',JSON.stringify(cartItems));
   },[cartItems]);
 
-  // useEffect(()=>{
-  //   let temp = 0;
-  //   cartItems?.forEarch((cartItem)=>{
-  //     temp = parseInt(cartItem.prize)  + temp;
-  //   })
-  //   setTotalAmount(temp);
-  // },[cartItems])
 
   useEffect(() => {
     let temp = 0;
@@ -40,7 +33,10 @@ function Cart() {
     })
     setTotalAmount(temp);
     //console.log(temp)
-  }, [cartItems])
+  }, [cartItems]);
+
+  const shippping = parseInt(100);
+  const grantTotal = shippping+totalAmount;
 
   return (
     <Layout >
@@ -87,7 +83,7 @@ function Cart() {
                   <p className="mt-1 text-base font-semibold text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>₹{prize}</p>
                 </div>
                 <div onClick={()=>deleteProductFromCart(item)}
-                   className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                   className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6 cursor-pointer">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                   </svg>
@@ -107,13 +103,13 @@ function Cart() {
             </div>
             <div className="flex justify-between">
               <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>Shipping</p>
-              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>₹20</p>
+              <p className="text-gray-700" style={{ color: mode === 'dark' ? 'white' : '' }}>₹{shippping}</p>
             </div>
             <hr className="my-4" />
             <div className="flex justify-between mb-3">
               <p className="text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>Total</p>
               <div className>
-                <p className="mb-1 text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>₹{totalAmount+20}</p>
+                <p className="mb-1 text-lg font-bold" style={{ color: mode === 'dark' ? 'white' : '' }}>₹{grantTotal}</p>
               </div>
             </div>
             <Modal  />
