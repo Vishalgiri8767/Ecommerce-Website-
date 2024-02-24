902212b6b0af332e42fac4526e19100597345679
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-02-24 13:02:45 +0530

    secured openai key by adding .env file

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,8 @@
 export const ROZORPAY_KEY = import.meta.env.VITE_ROZORPAY_KEY
 export const ROZORPAY_SECRET_KEY= import.meta.env.VITE_ROZORPAY_SECRET_KEY
-export const USER_AVATAR = "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?size=626&ext=jpg"
+export const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 
-export const OPENAI_API_KEY = "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";
+export const USER_AVATAR = "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?size=626&ext=jpg"
 
 // export const ROZORPAY_KEY = 'rzp_test_cYdyc8NJS0sqxu'
 // export const ROZORPAY_SECRET_KEY='5AMg8PInkrSvuQ7oxipukaWw'
