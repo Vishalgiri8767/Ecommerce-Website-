30941e0fd17758222e48f6f6eefe0e640c31c7cd
Vishalgiri8767 <vishalgiri8767@gmail.com>
2024-03-05 23:08:19 +0530

    Added functionality to display liked products

---
+++ src/pages/wishlist/Wishlist.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Wishlist = () => {
+  return (
+    <div>
+        This is wish list page.
+        here you see liked products.
+    </div>
+  )
+}
+
+export default Wishlist
--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,3 @@ import { defineConfig } from 'vite'
   plugins: [react()],
  })
 
-
-// export default defineConfig({
-
-//   build: {
-//           rollupOptions: {
-//               output:{
-//                   manualChunks(id) {
-//                       if (id.includes('node_modules')) {
-//                           return id.toString().split('node_modules/')[1].split('/')[0].toString();
-//                       }
-//                   }
-//               }
-//           }
-//       }
-//   });
